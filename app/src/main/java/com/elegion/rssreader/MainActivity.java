bf2a406b040827e663c5cb0d83306cb7e7d059e5
Artyom Ivanyuk <ivanyuk.artyom@gmail.com>
2014-09-10 20:57:41 +0400

    Loading

--- app/src/main/java/com/elegion/rssreader/MainActivity.java
+++ app/src/main/java/com/elegion/rssreader/MainActivity.java
@@ -1,36 +1,89 @@
 package com.elegion.rssreader;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private View mSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        mSendButton = findViewById(R.id.btn_load);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onResume() {
+        super.onResume();
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadData();
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        mSendButton.setOnClickListener(null);
+        super.onPause();
+    }
+
+    private String loadData() {
+        String result = "";
+        URL url;
+        try {
+            url = new URL("http://news.yandex.ru/auto_racing.rss");
+            URLConnection urlConnection = url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            try {
+                result = convertStream(in);
+            } finally {
+                in.close();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return super.onOptionsItemSelected(item);
+        return result;
+    }
+
+    public static final int EOF = -1;
+
+    public static String convertStream(@NonNull InputStream is) {
+        final StringBuilder result = new StringBuilder();
+        final Reader reader = new InputStreamReader(is, Charset.defaultCharset());
+        final char[] buffer = new char[4096];
+        try {
+            int bytes;
+            while ((bytes = reader.read(buffer)) != EOF) {
+                result.append(buffer, 0, bytes);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result.toString();
     }
 }
